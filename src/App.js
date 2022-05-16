a662dd3fecf7797e17b73dca892237a120d8eae1
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-16 14:06:42 -0400

    components imported

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
