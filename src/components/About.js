9fa307dc2ed79049c08b747ce00bb21fe8f2a00c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 16:42:35 -0400

    component created

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../styles/style.css';
+
+export default function About(props) {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor sit amet</p>
+    </div>
+  )
+};
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../styles/style.css';
+
+export default function Contact(props) {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>consectetur adipiscing elit</p>
+    </div>
+  )
+};
