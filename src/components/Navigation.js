a8a995add8be986e37e40f7af111658115cc47c4
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 16:45:40 -0400

    navlinks added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
-
+import { NavLink, Routes, Route } from 'react-router-dom';
 import '../styles/style.css';
 import '../styles/Navigation.css';
 
 export default function Navigation(props) {
   return (
-    <div>
+    <div className='navDiv'>
       <nav className='navbar'>
-        <ul className=''>
-          <li>nav1</li>
-          <li>nav1</li>
-          <li>nav1</li>
-          <li>nav1</li>
+        <ul className='nav'>
+          <li>
+            <NavLink to='about' className={''}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='portfolio' className={''}>
+              Portfolio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='contact' className={''}>
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='resume' className={''}>
+              Resume
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
