f02d5e96277d90be75997f2b8886ecf2eb10898b
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 16:43:08 -0400

    component created

---
+++ src/components/Resume.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../styles/style.css';
+
+export default function Resume() {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    </div>
+  )
+};
