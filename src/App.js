6e0a6ca5f430d18da0126a5378e35456ca2a3e85
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 16:47:15 -0400

    Routes component added: about/portf/contact/resume

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
+import { NavLink, Routes, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
-import Project from './components/Project';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Project />
+
+      <Routes>
+        <Route path='about' element={<About />} />
+        <Route path='portfolio' element={<Portfolio />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='resume' element={<Resume />} />
+      </Routes>
+      
       <Footer />
     </div>
   );
