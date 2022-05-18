209404b477d69aad55c9585ae6c960fb4238391c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 23:22:37 -0400

    component created, style path updated and linked

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import '../styles/style.css';
-import '../styles/Project.css';
+import '../css/style.css';
+import '../css/Project.css';
+
+import underConstruction from '../images/under-construction.jpg';
 
 export default function Project(props) {
   return (
-    <div>
-      <h3>Project</h3>
-    </div>
+    <figure className='project'>
+      <a href=''>
+        <img src={underConstruction} alt='image is nested in a link to the deployed app'/>
+      </a>
+      <figcaption>
+        <h3>Project Title</h3>
+        <p>Description</p>
+        <a href=''>Link to Repo</a>
+      </figcaption>
+    </figure>
   )
 };
