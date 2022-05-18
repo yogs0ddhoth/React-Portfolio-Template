857c85435db38dfed299cc2f4903a288de607ef5
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-17 23:22:06 -0400

    style linked, classes added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import '../css/style.css';
+import '../css/Portfolio.css';
 import Project from './Project';
 
 export default function Contact(props) {
   return (
-    <div>
+    <div className='portfolio'>
       <h2>Portfolio</h2>
       {/* TODO: create props to render individual projects */}
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
+      <div className='projects'>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
     </div>
   )
 };
