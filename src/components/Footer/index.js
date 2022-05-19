7bd84974886ef1b850ea2bd36dafcf06ee54529c
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-19 17:03:15 -0400

    changed directory

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../../css/style.css';
+import '../../css/Footer.css';
+
+export default function Footer(props) {
+  return (
+    <div className='footer'>
+      <h2>Footer</h2>
+    </div>
+  )
+};
