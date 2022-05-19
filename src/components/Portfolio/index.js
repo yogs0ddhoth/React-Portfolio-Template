33eb282090bdcf17348b952061b275be6b9d83cd
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-19 17:01:32 -0400

    configured styling with css utils, changed dir

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import '../../css/style.css';
+import '../../css/Portfolio.css';
+import Project from './Project';
+
+export default function Contact(props) {
+  return (
+    <div className='portfolio col-12 flex-row'>
+      <h2 className='title col-12 align-start'>Portfolio</h2>
+      {/* TODO: create props to render individual projects */}
+      <div className='projects align-start flex-row justify-space-around '>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </div>
+  )
+};
