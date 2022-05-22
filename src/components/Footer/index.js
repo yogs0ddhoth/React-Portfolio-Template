76ffcb0063b7b476545a3e8fbd3da6ef6defe255
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-21 23:26:26 -0400

    added links and icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,31 @@ import '../../css/Footer.css';
 
 export default function Footer(props) {
   return (
-    <div className='footer'>
-      <h2>Footer</h2>
+    <div className='footer col-12 pb-3'>
+      
+      <ul className='col-12 flex-row justify-center'>
+
+        <li className="icon email col-1 row justify-center">
+          <a href="mailto:">
+            <i className="fas fa-envelope fa-2x"></i>
+          </a>
+        </li>
+        <li className="icon col-1 row justify-center">
+          <a href="">
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+        </li>
+        <li className="icon col-1 row justify-center">
+          <a href="">
+            <i className="fab fa-stack-overflow fa-2x"></i>
+          </a>
+        </li>
+        <li className="icon col-1 row justify-center">
+          <a href="">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   )
 };
