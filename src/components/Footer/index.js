117814bd6797af5594007be6c450c286c66eeb15
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-23 16:12:58 -0400

    updated styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,28 +4,28 @@ import '../../css/Footer.css';
 
 export default function Footer(props) {
   return (
-    <div className='footer col-12 pb-3'>
+    <div className='footer col-12 pt-3'>
       
       <ul className='col-12 flex-row justify-center'>
 
-        <li className="icon email col-1 row justify-center">
-          <a href="mailto:">
-            <i className="fas fa-envelope fa-2x"></i>
+        <li className="icon email col-2 col-1-sm row justify-center">
+          <a href="mailto:" className='col-12 row'>
+            <i className="fas fa-envelope fa-2x align-start"></i>
           </a>
         </li>
-        <li className="icon col-1 row justify-center">
-          <a href="">
-            <i className="fab fa-github fa-2x"></i>
+        <li className="icon col-2 col-1-sm row justify-center">
+          <a href="" className='col-12 row'>
+            <i className="fab fa-github fa-2x align-start"></i>
           </a>
         </li>
-        <li className="icon col-1 row justify-center">
-          <a href="">
-            <i className="fab fa-stack-overflow fa-2x"></i>
+        <li className="icon col-2 col-1-sm row justify-center">
+          <a href="" className='col-12 row'>
+            <i className="fab fa-stack-overflow fa-2x align-start"></i>
           </a>
         </li>
-        <li className="icon col-1 row justify-center">
-          <a href="">
-            <i className="fab fa-linkedin fa-2x"></i>
+        <li className="icon col-2 col-1-sm row justify-center">
+          <a href="" className='col-12 row'>
+            <i className="fab fa-linkedin fa-2x align-start"></i>
           </a>
         </li>
       </ul>
