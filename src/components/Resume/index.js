8547214aebc2e259e08512986868d559dcd5a9ce
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-23 16:12:27 -0400

    linked download updated styling

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import '../../css/style.css';
 
+import underConstruction from '../../images/under-construction.jpg';
+
 export default function Resume() {
+  const ulStyle = { listStyleType: 'disc' }
   return (
-    <div>
-      <h2 className='title'>Resume</h2>
-      <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    <div className='col-12 flex-row'>
+      <a href={underConstruction} download={underConstruction} className='title col-12 align-start'>
+        <h2 className='title'>Resume</h2>
+      </a>
+      <div className='title col-12 flex-row align-start'>
+        <h3 className='col-12'>Proficiencies:</h3>
+        <ul className='col-12 '>
+          <li>sed do</li>
+          <li>eiusmod</li>
+          <li>tempor incididunt</li>
+          <li>ut labore</li>
+          <li>et dolore</li>
+          <li>magna aliqua</li>
+        </ul>
+      </div>
     </div>
   )
 };
