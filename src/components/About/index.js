e8efb8c47b61ab2256bcad6bf641837378c80f16
yogs0ddhoth <blin7.webdev@gmail.com>
2022-05-23 16:13:43 -0400

    updated styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import avatar from '../../images/avatar.png';
 import '../../css/style.css';
 
 export default function About(props) {
+  const imgStyle = { borderRadius: '50%' };
   return (
-    <div>
-      <h2 className='title'>About Me</h2>
+    <div className='col-12 flex-row'>
+      <h2 className='title col-12 align-start'>About Me</h2>
       {/*Todo: add generic avatar image - favicon?*/}
-      <img src='' alt='Avatar' />
-      <p>Lorem ipsum dolor sit amet</p>
+      <img src={avatar} alt='Avatar' className='col-3 col-2-md col-1-lg' style={imgStyle}/>
+      <p className='col-12'>Lorem ipsum dolor sit amet</p>
     </div>
   )
 };
